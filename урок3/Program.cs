f58a31c86ef5e8e2d2793dfcf33905a0ab5f0a80
Asia84 <84asia@mail.ru>
2022-07-12 16:04:19 +0700

    урок3 факториал с использованием типа double

---
+++ урок3/Program.cs
@@ -0,0 +1,42 @@
+﻿/*int Factorial (int n)//в 
+{
+    if(n==1) return 1;//   1! = 1(возвращает единицу), 0! = 1
+    else return n * Factorial(n - 1);
+
+}
+
+Console.WriteLine(Factorial(5));//1*2*3 = 6*/
+
+
+
+
+/*int Factorial (int n)//в 
+{
+    if(n==1) return 1;//   1! = 1(возвращает единицу), 0! = 1
+    else return n * Factorial(n - 1);
+
+}
+for (int i = 1; i < 40; i++)
+{
+//Console.WriteLine(Factorial(i));    // i меняется от 1 до 39
+Console.WriteLine($"{i}! = {Factorial(i)}");
+
+}
+*/
+
+
+
+
+
+double Factorial (int n)//в 
+{
+    if(n==1) return 1;//   1! = 1(возвращает единицу), 0! = 1
+    else return n * Factorial(n - 1);
+
+}
+for (int i = 1; i < 40; i++)
+{
+//Console.WriteLine(Factorial(i));    // i меняется от 1 до 39
+Console.WriteLine($"{i}! = {Factorial(i)}");
+
+}
